04a77f779836bcb38ec7a4e8ce38eaf0841070eb
sebastian casagua <joseca2398@hotmail.com>
2023-05-27 17:15:07 -0500

    Create a EmpleadoDTO

--- test/src/main/java/com/parameta/test/dto/EmpleadoDTO.java
+++ test/src/main/java/com/parameta/test/dto/EmpleadoDTO.java
@@ -1,5 +1,48 @@
 package com.parameta.test.dto;
 
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * DTO que representa el Empleado
+ * 
+ * @author JohanCasagua
+ *
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class EmpleadoDTO {
 
+	/** Employee name */
+	private String nombre;
+	
+	/** Employee last name */
+	private String pellido;
+	
+	/** Document type */
+	private String tipoDocumento;
+	
+	/** Document number */
+	private String numeroDocumento;
+	
+	/** Birth date*/
+	private Date fechaNacimiento;
+	
+	/** Date of joining the company*/
+	private Date vinculacionCompania;
+	
+	/** Position */
+	private String cargo;
+	
+	/** Salary */
+	private Double salario;
+	
 }
